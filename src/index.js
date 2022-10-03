c4b82cdf4ae3d2c2c1806318ed2dcfc4ed794257
Kay Bi <kaybi9x@gmail.com>
2022-10-03 16:07:18 +0700

    add

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//const name = "my name";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+//Su dung React.createElement
+//root.render(
+//  React.createElement("h1", { style: { textAlign: "center" } }, name)
+//)
+
+//Su dung JSX
+//root.render(<h1 style={{ textAlign: "center" }}>{name}</h1>);
+
+//Tao Element the hien danh sach cac loai qua su dung JSX
+// const fruits = ['Apple', 'Banana', 'Orange', 'Apricot', 'Black rowan', 'Cranberry'];
+// root.render(
+//   <div>
+//     <h1>List of fruits</h1>
+//   <ul>
+//       {fruits.map((item) => (
+//         <li>{item}</li>
+//       ))}
+//   </ul>
+//   </div>
+// )
+
+//Hien thi tgian hien tai bang JSX
+const tick = () => {
+  root.render(
+    <div>
+      <h1>Hello, world!</h1>
+      <h3>It is {new Date().toLocaleTimeString()}.</h3>
+    </div>
+  );
+};
+setInterval(tick, 1000);
